f385d15dc9dd5e95610e9dc97d1607e3165ba199
Ayo-faks <ayoolafakoya@gmail.com>
2024-08-19 11:40:14 +0000

    changed logo, fonts

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,8 +12,7 @@ const About = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="mb-12 max-w-[540px] lg:mb-0">
                 <h2 className="mb-5 text-3xl font-bold leading-tight text-dark dark:text-white sm:text-[40px] sm:leading-[1.2]">
-                  Brilliant Toolkit to Build Next.js SaaS Websites.
-                </h2>
+                Meet Wulo: Your AI-Powered Legal Partner for Smarter, Faster, and More Accurate Workflows                </h2>
                 <p className="mb-10 text-base leading-relaxed text-body-color dark:text-dark-6">
                 Wulo is an AI-powered legal assistant designed to revolutionize your legal workflow. With features like advanced AI-driven research, intelligent analytics, exceptional Q&A capabilities, and automated process writing. 
                   <br /> <br />
